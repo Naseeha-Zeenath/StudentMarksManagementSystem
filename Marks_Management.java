ceafa9c3885e2dce1f2e43b061a1cf7c0357f40f
nzeenath <nzeenath2002@gmail.com>
2026-06-17 16:57:28 +0530

    3rd commit

--- Marks_Management.java
+++ Marks_Management.java
@@ -3,6 +3,146 @@ class Student{
 	String id;
 	String name;
 	int prfMarks;
+	int dbmsMarks;
+	
+	Student(){}
+	
+	Student(String id, String name, int prfMarks, int dbmsMarks){
+		this.id = id;
+		this.name = name;
+		this.prfMarks = prfMarks;
+		this.dbmsMarks = dbmsMarks;
+	}
+	
+}
+
+class Marks_Management{
+	public static Student[] st = new Student[0];
+	
+	public static void addStudent(){
+		increaseStudent();
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter Student Id         : ");
+		String id = input.next();
+		System.out.print("Enter Student Name       : ");
+		String name = input.next();
+		
+		int prfMarks;
+		int dbmsMarks;
+		do{
+			System.out.print("Enter Student PRF Marks  : ");
+			prfMarks = input.nextInt();
+			
+		}while(prfMarks<0);
+		
+		do{
+			System.out.print("Enter Student DBMS Marks : ");
+			dbmsMarks = input.nextInt();
+		}while(dbmsMarks<0);
+		
+		st[st.length-1] = new Student(id,name,prfMarks,dbmsMarks);
+		
+	}
+	
+	public static void increaseStudent(){
+		Student[] temp = new Student[st.length+1];
+		
+		for(int i =0; i<st.length; i++){
+			temp[i] = st[i];
+		}
+		st = temp;
+	}
+	public static void deleteStudent(){
+		int value =searchStudent();
+		
+		if(value >= 0){
+			for(int j =value; j< st.length-1;j++){
+				st[j] = st[j+1];
+			}
+			
+			Student[] temp = new Student[st.length-1];	
+			for(int i = 0; i<temp.length; i++){
+				temp[i] = st[i];
+			}
+			
+			st = temp;
+		
+		}
+		
+		System.out.print("============================\n");
+		
+		for(int i=0; i<st.length; i++){
+			System.out.println(st[i].id+"\t"+st[i].name+"\t"+st[i].prfMarks+"\t"+st[i].dbmsMarks);
+		}
+		
+		
+	}
+	
+	public static int searchStudent(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Search by \nId (Enter number 1) \nName(Enter number 2)");
+		System.out.print("Enter option : ");
+		int option = input.nextInt();
+		
+		String id= null;
+		String name = null;
+		
+		if(option == 1){
+			System.out.print("Enter Student Id : ");
+			id = input.next();
+		}else if(option == 2){
+			System.out.print("Enter Student Name : ");
+			name = input.next();
+			
+		}else{
+			System.out.print("Invalid option...");
+			
+		}
+		int value = -1;
+		
+		for(int i=0; i<st.length; i++){
+			if((st[i].id).equals(id) || (st[i].name).equals(name)){
+				System.out.println(st[i].id+"\t"+st[i].name+"\t"+st[i].prfMarks+"\t"+st[i].dbmsMarks);
+				value = i;
+			}
+		}
+		
+		return value;
+	}
+	
+	public static void printStudent(){
+		System.out.print("============================\n");
+		for(int i=0; i<st.length; i++){
+			System.out.println(st[i].id+"\t"+st[i].name+"\t"+st[i].prfMarks+"\t"+st[i].dbmsMarks);
+		}
+	}
+	
+	public static void main(String args[]){
+		addStudent();
+		addStudent();
+		addStudent();
+		printStudent();
+		int search = searchStudent();
+		//updateStudent();
+		deleteStudent();
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+/*class Student{
+	String id;
+	String name;
+	int prfMarks;
 	int dbmsMarks;	
 	
 	public void print(){
@@ -61,3 +201,4 @@ class Marks_Management{
 			}
 		}	
 }
+*/
